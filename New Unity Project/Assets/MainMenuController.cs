eb5c9c1609b7aa2915328e8c8ee71df6aecc16b1
Sebastian Markiton <smarkiton1@gmail.com>
2019-12-09 22:09:31 +0100

    Add basics project files

---
+++ New Unity Project/Assets/MainMenuController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] Button firstButton;
+    [SerializeField] Button secondButton;
+    [SerializeField] Button thirdButton;
+
+    protected virtual void OnEnable()
+    {
+        Debug.Log("OnEnable");
+        SubscribeToEvents();
+    }
+
+    protected virtual void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        UnsubscribeToEvents();
+    }
+
+    private void SubscribeToEvents()
+    {
+        Debug.Log("SubscribeToEvents");
+
+        firstButton.onClick.AddListener(OnFirstButtonClick);
+        secondButton.onClick.AddListener(OnSecondButtonClick);
+        thirdButton.onClick.AddListener(OnThirdButtonClick);
+    }
+
+    private void UnsubscribeToEvents()
+    {
+        Debug.Log("UnsubscribeToEvents");
+
+        firstButton.onClick.RemoveListener(OnFirstButtonClick);
+        secondButton.onClick.RemoveListener(OnSecondButtonClick);
+        thirdButton.onClick.RemoveListener(OnThirdButtonClick);
+    }
+
+    private void OnFirstButtonClick()
+    {
+        Debug.Log("OnFirstButtonClick");
+        Screens.Instance.SwitchScreens();
+    }
+
+    private void OnSecondButtonClick()
+    {
+        Debug.Log("OnSecondButtonClick");
+    }
+
+    private void OnThirdButtonClick()
+    {
+        Debug.Log("OnThirdButtonClick");
+    }
+}
---
+++ New Unity Project/Assets/Screens.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Screens : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject mainMenuScreen;
+    [SerializeField] private GameObject secondMenuScreen;
+
+    private static Screens instance;
+
+    public static Screens Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<Screens>();
+            }
+
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SwitchScreens()
+    {
+        bool isMainMenuActive = mainMenuScreen.activeSelf;
+        mainMenuScreen.SetActive(!isMainMenuActive);
+        secondMenuScreen.SetActive(isMainMenuActive);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //
+    }
+}
